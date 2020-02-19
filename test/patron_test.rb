6e53deb38362e0156696eb0dadadb587baec8a20
Alex Lynch <mlynch5187@gmail.com>
2020-02-19 09:18:21 -0700

    Complete 1st iteration

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,23 +12,17 @@ class PatronTest < MiniTest::Test
   def test_patrons_exist
     assert_instance_of Patron, @patron_1
   end
+
+  def test_patron_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
+  end
+
+  def test_patron_interests_can_be_added
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
 end
-
-
-
-# pry(main)> patron_1.name
-# # => "Bob"
-#
-# pry(main)> patron_1.spending_money
-# # => 20
-#
-# pry(main)> patron_1.interests
-# # => []
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_1.interests
-# # => ["Dead Sea Scrolls", "Gems and Minerals"]
-# ```

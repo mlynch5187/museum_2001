bfafdbb42210f02dbbef74b13b1d85bc5a5cf73a
Alex Lynch <mlynch5187@gmail.com>
2020-02-19 10:18:17 -0700

    Complete 2nd iteration

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,6 +1,18 @@
 class Museum
+  attr_reader :name, :exhibits
 
   def initialize(name)
+    @name = name
+    @exhibits = []
+  end
 
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    exhibit_names = exhibits.map {|exhibit| exhibit.name}
+    exhibits_of_no_interest = exhibit_names - patron.interests
+    exhibit_names - exhibits_of_no_interest
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -8,53 +8,39 @@ class MuseumTest < MiniTest::Test
 
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   def test_museum_exists
     assert_instance_of Museum, @dmns
   end
+
+  def test_museum_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_exhibits_can_be_added_to_museum
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_exhibits_are_recommended_based_upon_interests_of_patron
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
+    assert_equal ["Gems and Minerals", "Dead Sea Scrolls"], @dmns.recommend_exhibits(@patron_1)
+    assert_equal ["IMAX"], @dmns.recommend_exhibits(@patron_2)
+  end
 end
-
-
-# pry(main)> dmns.name
-# # => "Denver Museum of Nature and Science"
-#
-# pry(main)> dmns.exhibits
-# # => []
-#
-# pry(main)> gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-# # => #<Exhibit:0x00007fb400bbcdd8...>
-#
-# pry(main)> dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-# # => #<Exhibit:0x00007fb400b851f8...>
-#
-# pry(main)> imax = Exhibit.new({name: "IMAX",cost: 15})
-# # => #<Exhibit:0x00007fb400acc590...>
-#
-# pry(main)> dmns.add_exhibit(gems_and_minerals)
-#
-# pry(main)> dmns.add_exhibit(dead_sea_scrolls)
-#
-# pry(main)> dmns.add_exhibit(imax)
-#
-# pry(main)> dmns.exhibits
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>, #<Exhibit:0x00007fb400acc590...>]
-#
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fb400a51cc8...>
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_2 = Patron.new("Sally", 20)
-# # => #<Patron:0x00007fb400036338...>
-#
-# pry(main)> patron_2.add_interest("IMAX")
-#
-# pry(main)> dmns.recommend_exhibits(patron_1)
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
-#
-# pry(main)> dmns.recommend_exhibits(patron_2)
-# # => [#<Exhibit:0x00007fb400acc590...>]
-# ```
